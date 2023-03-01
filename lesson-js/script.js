5f4f0b9ca3ad518532dc3114a6105226962884c5
giomikadze <110746808+giomikadze@users.noreply.github.com>
2023-03-01 19:30:40 +0100

    js-2

---
+++ lesson-js/script.js
@@ -0,0 +1,70 @@
+
+
+// произведение элементов массива
+
+
+// const array = [7,3,6,9,4,5];
+// let multiplication = 1;
+// for (let i = 0; i < array.length; i++) {
+//   multiplication *= array[i];
+// }
+// console.log(multiplication);
+
+
+// суммa квадратов элементов массива
+
+// let squares = 0;
+// for (let i = 0; i < array.length; i++) {
+//   let res = array[i] ** 2;
+//   squares += res;
+// }
+// console.log(squares);
+
+// максимальное значение массивa
+
+
+// let maxValue = 0;
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > maxValue) {
+//   maxValue = array[i];
+//  }
+// }
+// console.log(maxValue);
+
+
+
+// минимальное значение в массиве
+
+// let minValue = array[0];
+//  for (let i = 0; i < array.length; i++) {
+//     if (array[i] < minValue) {
+//       minValue= array[i];
+//   }
+// }
+//  console.log(minValue);
+
+
+// среднее значение элементов массива
+
+// let sum = 0;
+// let sumOfAll = array.length;
+// let average = 0;
+// for (let i = 0; i < array.length; i++) {
+//   sum += array[i]
+//   average = sum / sumOfAll;
+// }
+// console.log(average);
+
+
+
+// количество положительных элементов в массиве
+
+// let sumOfPositive = 0;
+// for (let i = 0; i <array.length; i++) {
+//     if (array[i] > 0) {
+//         sumOfPositive += 1;
+//     }
+// }
+// console.log(sumOfPositive);
+
+
